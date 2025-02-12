b3bdebc1c1427124deb8a4a3134768ba6cea9777
Wiki OwSki <azad.ali@laplateforme.io>
2025-02-12 16:19:38 +0100

    update of the code and removed some unnecessary

---
+++ main_fight.py
@@ -0,0 +1,31 @@
+import pygame
+import random
+
+def damage():
+
+class Combat:
+    def __init__(self, player_pokemon, enemy_pokemon):
+        self.player_pokemon = player_pokemon
+        self.enemy_pokemon = enemy_pokemon
+        self.player_turn = True
+        self.running = True
+
+    
+     def damage(self, attacker, defender):
+        return attacker.attack_pokemon(defender)
+
+
+    def get_pokedex():
+
+class Menu:
+    def __init__(self):
+        self = self
+
+    def start_game():
+
+    def add_pokemon():
+
+    def get_pokedex():
+
+pygame.quit()
+sys.exit()
