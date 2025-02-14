53cc9eccdb1fcdc4c56396feb647ef99841ab0d1
Wiki OwSki <azad.ali@laplateforme.io>
2025-02-14 14:46:45 +0100

    fixed the files being messy

--- main_fight.py
+++ main_fight.py
@@ -3,6 +3,7 @@ import random
 import sys
 
 def damage():
+    pass
 
 class Combat:
     def __init__(self, player_pokemon, enemy_pokemon):
@@ -12,32 +13,25 @@ class Combat:
         self.running = True
 
     
-     def damage(self, attacker, defender):
+    def damage(self, attacker, defender):
         return attacker.attack_pokemon(defender)
 
 
     def get_pokedex():
+        pass
 
 class Menu:
     def __init__(self):
         self = self
 
     def start_game():
+        pass
 
     def add_pokemon():
+        pass
 
-    # Function to get the Pokédex data
     def get_pokedex():
-        return {
-            "Pikachu": {"Type": "Electric", "Attack": 55, "Defense": 40, "HP": 35},
-            "Bulbasaur": {"Type": "Grass", "Attack": 49, "Defense": 49, "HP": 45},
-            "Charmander": {"Type": "Fire", "Attack": 52, "Defense": 43, "HP": 39},
-            "Squirtle": {"Type": "Water", "Attack": 48, "Defense": 65, "HP": 44},
-        }
-
-    # Initialize the Pokédex
-    pokedex = get_pokedex()
-
+        pass
 
 pygame.quit()
 sys.exit()
